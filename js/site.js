a28f0abe36f9764ca04008cbee380f22d7f6e421
tlegendmod <lukowtim@gmail.com>
2020-05-07 01:30:33 -0500

    updates

--- js/site.js
+++ js/site.js
@@ -10,13 +10,19 @@ function CreateNewIdea () {
     btn.id = "buttonid";
     btn.className = "ideabutton"
     btn.innerHTML = "Click";
-    btn.onclick = "MoveOnClick()";
+    btn.onclick = MoveOnClick();
     document.getElementById(idea.id).appendChild(btn);
 }
 
 function MoveOnClick () {
     // when click and hold is active
-    
+    let ideas = document.getElementsByClassName("idea").getElementById(idea.id);
+    ideas.onmousedown = function(event) {
+        //fires when mouse down on button
+        console.log("clicking and holding on div");
+    }
+
+
     // move the div element freely to the edges of the viewport
     // release clickhold to set new position of div element
 }
