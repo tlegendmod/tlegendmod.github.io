cf2b74b69bfe988ece24fcc22b1a96147d0f7a40
tlegendmod <lukowtim@gmail.com>
2020-05-07 00:59:10 -0500

    updates

--- js/site.js
+++ js/site.js
@@ -4,21 +4,13 @@
 function CreateNewIdea () {
     var idea = document.createElement("div");
     idea.id = "idea";
-    idea.className = "idea style";
+    idea.className = "idea";
     document.body.appendChild(idea);
     var btn = document.createElement("button");
     btn.id = "buttonid";
     document.getElementById(idea.id).appendChild(btn);
 
-    /* 
-    if(document.body != null) {
-    document.body.appendChild(div);
-    }
-    
-    else {
-    console.log("DIV NOT APPENDED TO BODY");
-    };
-    */
+
 }
 
 function MoveOnClick () {
