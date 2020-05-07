9bca92589decdcfc41e34e455aa9b0550fff7ef2
tlegendmod <lukowtim@gmail.com>
2020-05-07 01:07:08 -0500

    updates

--- js/site.js
+++ js/site.js
@@ -8,9 +8,9 @@ function CreateNewIdea () {
     document.body.appendChild(idea);
     var btn = document.createElement("button");
     btn.id = "buttonid";
+    btn.className = "ideabutton"
     btn.innerHTML = "Click";
     document.getElementById(idea.id).appendChild(btn);
-
 
 }
 
