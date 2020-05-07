dcebaf5b507e94a2938dfb279ce3b966e3eacd81
tlegendmod <lukowtim@gmail.com>
2020-05-07 03:02:45 -0500

    updates

--- js/site.js
+++ js/site.js
@@ -27,7 +27,7 @@ function CreateIdeaSpace() {
     var SpaceArea = document.createElement("div");
     SpaceArea.id = "spacearea";
     SpaceArea.className = "ideaspacearea";
-    document.body.insertBefore(document.body.firstChild(),SpaceArea);
+    document.body.insertBefore(document.body.firstElementChild,SpaceArea);
 }
 
 // Make the DIV element draggable:
